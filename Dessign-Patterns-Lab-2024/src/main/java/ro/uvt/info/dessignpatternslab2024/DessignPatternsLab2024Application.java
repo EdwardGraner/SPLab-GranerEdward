6fa767ba75fdc230591ae16dad6dd582d5c574c9
Graner Edward <118376938+EdwardGraner@users.noreply.github.com>
2025-02-10 12:57:28 +0200

    Project Created

---
+++ Dessign-Patterns-Lab-2024/src/main/java/ro/uvt/info/dessignpatternslab2024/DessignPatternsLab2024Application.java
@@ -0,0 +1,13 @@
+package ro.uvt.info.dessignpatternslab2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DessignPatternsLab2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DessignPatternsLab2024Application.class, args);
+    }
+
+}
---
+++ Dessign-Patterns-Lab-2024/src/test/java/ro/uvt/info/dessignpatternslab2024/DessignPatternsLab2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.uvt.info.dessignpatternslab2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DessignPatternsLab2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
